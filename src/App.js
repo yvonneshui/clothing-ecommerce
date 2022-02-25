ea27780005c159e24ce7af86dd89807206558151
yvonneshui <yvonne.shui126@gmail.com>
2022-02-25 11:41:11 -0500

    feature:header component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import ShopPage from "./pages/ShopPage";
+import Header from "./components/header/Header";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={ShopPage} />
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import './Header.styles.scss'
+import {ReactComponent as Logo} from '../../assets/crown.svg'
+function Header() {
+	return (
+		<div className="header">
+			<Link className='logo-container' to='/'>
+				<Logo className='logo'/>
+			</Link>
+			<div className="options">
+				<Link className='option' to='./shop'>
+					Shop
+				</Link>
+				<Link className='option' to='./shop'>
+					Contact
+				</Link>
+			</div>
+		</div>
+		)
+}
+
+export default Header
