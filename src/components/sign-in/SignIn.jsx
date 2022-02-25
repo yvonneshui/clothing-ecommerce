ce7ec8dbd58751bfe7c2b1fba210a64c1335c5cd
yvonneshui <yvonne.shui126@gmail.com>
2022-02-25 14:50:09 -0500

    feature:input form for password and email

--- src/components/sign-in/SignIn.jsx
+++ src/components/sign-in/SignIn.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './SignIn.styles.scss'
-
+import FormInput from '../form-input/FormInput'
 export default class SignIn extends Component {
 	constructor(){
 		super()
@@ -32,9 +32,22 @@ export default class SignIn extends Component {
 
 				<form>
 					<label>Email:</label>
-					<input type="email" name='email' value={this.state.email} onChange={this.handleChange} required/>
+					<FormInput 
+					type="email"
+					name='email' 
+					label='email'
+					value={this.state.email} 
+					handleChange={this.handleChange} 
+					required/>
+
 					<label>Password:</label>
-					<input type="password" name='password' value={this.state.password} onChange={this.handleChange}  required/>
+					<FormInput 
+					type="password" 
+					name='password' 
+					label='password'
+					value={this.state.password} 
+					handleChange={this.handleChange}  
+					required/>
 
 					<input type='submit' value='submit Form'/>
 				</form>
