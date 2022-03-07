75488c1b689dd43265627c0d7557fc27174f74e2
yvonneshui <yvonne.shui126@gmail.com>
2022-03-07 15:22:02 -0500

    feature: update cartUtils to update cart item quantity and changed shop data

--- src/components/collection-item/CollectionItem.jsx
+++ src/components/collection-item/CollectionItem.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import './CollectionItem.styles.scss'
+import Button from '../custom-button/Button'
+import {connect} from 'react-redux'
+import {addItem} from '../../redux/cart/cart-action'
 
-function CollectionItem({id,name,price,imageUrl}) {
+function CollectionItem({item,addItem}) {
+	const {name,price,imageUrl}=item
 	return (
 		<div className="collection-item">
 			<div 
@@ -12,11 +16,15 @@ function CollectionItem({id,name,price,imageUrl}) {
 			/>
 			<div className="collection-footer">
 				<span className='name'>{name}</span>
-				<span className='price'>{price}</span>
-
+				<span className='price'>${price}</span>
 			</div>
+			<Button inverted onClick={()=>addItem(item)}>Add to Cart</Button>
 		</div>
 	)
 }
 
-export default CollectionItem
+const mapDispatchToProps=(dispatch)=>({
+	addItem:item=>dispatch(addItem(item))
+})
+
+export default connect(null,mapDispatchToProps)(CollectionItem)
